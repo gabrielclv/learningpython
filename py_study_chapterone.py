25e64af135c891c84bb051030b2dd1d25826b1a8
Gabriel Cauã <gabrielcaualealvieira@gmail.com>
2025-08-13 17:25:32 -0300

    Add files via upload

---
+++ py_study_chapterone.py
@@ -0,0 +1,11 @@
+"1- Instale o Python."
+"2- Instale um editor texto (VScode,etc)"
+"3- Execute o  comando no prompt de comando(cmd): python (se a instalação tiver dado certo aparecerá: (>>>) )"
+
+print("Meu nome é Gabriel Cauã") 
+"execute o comando da linha acima no editor de texto ou no terminal(cmd)"
+
+"entrar no programa e executar no próprio prompt de comando:"
+" 1. cd + nome da pasta do programa "
+" 2. dir (ver os arquivos que estão dentro) "
+" 3. python + nome da file do programa "
